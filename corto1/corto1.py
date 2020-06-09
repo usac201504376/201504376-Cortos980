37d2fce9937c5b53053666556dd3c4cf33a75192
usac201504376 <2767620100901@ingenieria.usac.edu.gt>
2020-06-09 12:43:33 -0600

    commit final

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,7 +1,14 @@
 N = 0
 def Secuencia(N):
     for numero in range(2,376):
-        print (numero)
+        while numero > 1:
+            if numero%2 == 0:
+                num = [numero/2]
+                collatz = [] + num
+            else:
+                num2 = [3*n + 1]
+                collatz = [] + num2
+                print collatz
     
     return N
 
